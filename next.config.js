6564be13ed250a86fbfb9faa3fc2adf3089651da
Eytan Seidman <eytans@gmail.com>
2020-05-07 21:13:14 -0400

    updating files

---
+++ next.config.js
@@ -0,0 +1,8 @@
+//Please enter your test or production keys from the Stripe Dashboard and set them here
+
+module.exports = {
+  env: {
+    STRIPE_PUBLISHABLE_KEY: 'pk_test_YOUR_PUBLIC_KEY',
+    STRIPE_SECRET_KEY: 'sk_test_YOUR_SECRET_KEY,
+  },
+}
